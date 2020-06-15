ca79caf532958de851abb47923f0672f4bc04761
sahuang <xiaohai.xu@zilliz.com>
2020-06-15 10:25:51 +0800

    Update to SDK 0.8.2
    Signed-off-by: sahuang <xiaohai.xu@zilliz.com>

--- src/main/java/io/milvus/client/MilvusClient.java
+++ src/main/java/io/milvus/client/MilvusClient.java
@@ -26,9 +26,9 @@ import java.util.List;
 /** The Milvus Client Interface */
 public interface MilvusClient {
 
-  String clientVersion = "0.8.1";
+  String clientVersion = "0.8.2";
 
-  /** @return current Milvus client version： 0.8.1 */
+  /** @return current Milvus client version： 0.8.2 */
   default String getClientVersion() {
     return clientVersion;
   }
