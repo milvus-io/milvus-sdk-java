6c361047b6ec1c9d062a0bfec06d53405415e1b3
luhongwei <hongweilu@sohu-inc.com>
2021-08-28 14:33:32 +0800

    framework && simple connection

---
+++ src/main/java/io/milvus/client/AbstractMilvusGrpcClient.java
@@ -0,0 +1,43 @@
+package io.milvus.client;
+
+import io.grpc.StatusRuntimeException;
+import io.milvus.grpc.BoolResponse;
+import io.milvus.grpc.ErrorCode;
+import io.milvus.grpc.HasCollectionRequest;
+import io.milvus.grpc.MilvusServiceGrpc;
+import io.milvus.param.R;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+public abstract class AbstractMilvusGrpcClient implements MilvusClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(AbstractMilvusGrpcClient.class);
+
+    protected abstract MilvusServiceGrpc.MilvusServiceBlockingStub blockingStub();
+
+    protected abstract MilvusServiceGrpc.MilvusServiceFutureStub futureStub();
+
+    @Override
+    public R<Boolean> hasCollection(String collectionName) {
+        HasCollectionRequest hasCollectionRequest = HasCollectionRequest.newBuilder()
+                .setCollectionName(collectionName)
+                .build();
+
+        BoolResponse response;
+        try {
+            response = blockingStub().hasCollection(hasCollectionRequest);
+        } catch (StatusRuntimeException e) {
+            logger.error("[milvus] hasCollection:{} request error: {}", collectionName, e.getMessage());
+            return R.failed("rpc request failed");
+        }
+        Boolean aBoolean = Optional.ofNullable(response)
+                .map(BoolResponse::getValue)
+                .orElse(false);
+
+        return R.success(aBoolean);
+
+
+    }
+}
---
+++ src/main/java/io/milvus/client/MilvusServiceClient.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.milvus.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.milvus.grpc.*;
+import io.milvus.param.ConnectParam;
+import io.milvus.param.R;
+
+import java.util.concurrent.TimeUnit;
+
+public class MilvusServiceClient extends AbstractMilvusGrpcClient {
+
+    private final ManagedChannel channel;
+    private final MilvusServiceGrpc.MilvusServiceBlockingStub blockingStub;
+    private final MilvusServiceGrpc.MilvusServiceFutureStub futureStub;
+
+    public MilvusServiceClient(ConnectParam connectParam) {
+        channel = ManagedChannelBuilder.forAddress(connectParam.getHost(), connectParam.getPort())
+                .usePlaintext()
+                .maxInboundMessageSize(Integer.MAX_VALUE)
+                .keepAliveTime(connectParam.getKeepAliveTimeMs(), TimeUnit.MILLISECONDS)
+                .keepAliveTimeout(connectParam.getKeepAliveTimeoutMs(), TimeUnit.MILLISECONDS)
+                .keepAliveWithoutCalls(connectParam.isKeepAliveWithoutCalls())
+                .idleTimeout(connectParam.getIdleTimeoutMs(), TimeUnit.MILLISECONDS)
+                .build();
+        blockingStub = MilvusServiceGrpc.newBlockingStub(channel);
+        futureStub = MilvusServiceGrpc.newFutureStub(channel);
+    }
+
+    @Override
+    protected MilvusServiceGrpc.MilvusServiceBlockingStub blockingStub() {
+        return this.blockingStub;
+    }
+
+    @Override
+    protected MilvusServiceGrpc.MilvusServiceFutureStub futureStub() {
+        return this.futureStub;
+    }
+
+    @Override
+    public void close(long maxWaitSeconds) {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        ConnectParam build = ConnectParam.Builder.newBuilder()
+                .withHost("localhost")
+                .withPort(19530)
+                .build();
+
+        MilvusServiceClient milvusServiceClient = new MilvusServiceClient(build);
+
+        R<Boolean> chuwutest = milvusServiceClient.hasCollection("chuwutest");
+
+        System.out.println(chuwutest);
+    }
+
+
+}
+
