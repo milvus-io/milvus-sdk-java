940c3cc15f04234581b553b506f5deef33d7ffa9
trovwu <89676996+trovwu@users.noreply.github.com>
2021-08-29 23:29:54 +0800

    framework && simple connection (#191)
    * framework && simple connection
    
    * pass in exception message in R
    
    * pass in exception message in R
    
    * pass in exception message in R
    
    Co-authored-by: luhongwei <hongweilu@sohu-inc.com>

---
+++ src/main/java/io/milvus/param/R.java
@@ -0,0 +1,62 @@
+package io.milvus.param;
+
+import io.milvus.grpc.ErrorCode;
+
+public class R<T> {
+    private Exception exception;
+    private Integer status;
+    private T data;
+
+    public Exception getException() {
+        return exception;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public static <T> R<T> failed(Exception exception){
+        R<T> r = new R<>();
+        r.setStatus(-1);
+        r.setException(exception);
+        return r;
+    }
+
+    public static <T> R<T> failed(ErrorCode errorCode){
+        R<T> r = new R<>();
+        r.setStatus(errorCode.ordinal());
+        r.setException(new Exception(errorCode.name()));
+        return r;
+    }
+
+    public static <T> R<T> success(){
+        R<T> r = new R<>();
+        r.setStatus(0);
+        return r;
+    }
+
+
+    public static <T> R<T> success(T data){
+        R<T> r = new R<>();
+        r.setStatus(0);
+        r.setData(data);
+        return r;
+    }
+
+}
