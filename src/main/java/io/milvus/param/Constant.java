3b932f736617325c883809b9fd50a4135c76253f
luhongwei <hongweilu@sohu-inc.com>
2021-09-03 17:50:01 +0800

    draft of review code
    Signed-off-by: luhongwei <hongweilu@sohu-inc.com>

---
+++ src/main/java/io/milvus/param/Constant.java
@@ -0,0 +1,12 @@
+package io.milvus.param;
+
+public class Constant {
+
+    // default value for search key
+    public static String vectorTag = "$0";
+    public static String vectorFiled = "anns_field";
+    public static String topK = "topk";
+    public static String metricType = "metric_type";
+    public static String parmas = "params";
+
+}
