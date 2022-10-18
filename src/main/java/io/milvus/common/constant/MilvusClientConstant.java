9ce3ada6a528dfc1ce771edf83051a49a0884f70
xun.huang@zilliz.com <xun.huang@zilliz.com>
2022-10-18 19:59:41 +0800

    covert connect param host delete start http:// or https://.
    Signed-off-by: xun.huang@zilliz.com <xun.huang@zilliz.com>

---
+++ src/main/java/io/milvus/common/constant/MilvusClientConstant.java
@@ -0,0 +1,10 @@
+package io.milvus.common.constant;
+
+public class MilvusClientConstant {
+
+    public static class MilvusConsts {
+        public final static String HOST_HTTPS_PREFIX = "https://";
+
+        public final static String HOST_HTTP_PREFIX = "http://";
+    }
+}
