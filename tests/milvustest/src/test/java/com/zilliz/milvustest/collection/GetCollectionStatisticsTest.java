c9f9750b908652234b09133bede6820b7ca9f60d
yongpengli-z <yongpeng.li@zilliz.com>
2023-04-14 21:52:25 +0800

    modify testcase for getCollectionStatistics (#470)
    Signed-off-by: yongpengli-z <yongpeng.li@zilliz.com>

--- tests/milvustest/src/test/java/com/zilliz/milvustest/collection/GetCollectionStatisticsTest.java
+++ tests/milvustest/src/test/java/com/zilliz/milvustest/collection/GetCollectionStatisticsTest.java
@@ -2,20 +2,41 @@ package com.zilliz.milvustest.collection;
 
 import com.zilliz.milvustest.common.BaseTest;
 import com.zilliz.milvustest.common.CommonData;
+import com.zilliz.milvustest.common.CommonFunction;
 import io.milvus.grpc.GetCollectionStatisticsResponse;
 import io.milvus.param.R;
+import io.milvus.param.collection.DropCollectionParam;
 import io.milvus.param.collection.GetCollectionStatisticsParam;
+import io.milvus.param.dml.InsertParam;
 import io.milvus.response.GetCollStatResponseWrapper;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 @Epic("Collection")
 @Feature("GetCollectionStatistics")
 public class GetCollectionStatisticsTest extends BaseTest {
+
+  public String commonCollection;
+
+  @BeforeClass(description = "Create collection before test",alwaysRun=true)
+  public void provideCollectionName() {
+    String newCollection = CommonFunction.createNewCollection();
+    commonCollection = newCollection;
+    List<InsertParam.Field> fields = CommonFunction.generateData(2000);
+    CommonFunction.insertDataIntoCollection(newCollection,fields);
+  }
+  @AfterClass(description = "drop collection before test",alwaysRun=true)
+  public void dropCollection() {
+    milvusClient.dropCollection(DropCollectionParam.newBuilder().withCollectionName(commonCollection).build());
+  }
 
   @Severity(SeverityLevel.BLOCKER)
   @Test(description = "Shows the statistics information of a collection.",groups = {"Smoke"})
@@ -24,7 +45,7 @@ public class GetCollectionStatisticsTest extends BaseTest {
         milvusClient
             .getCollectionStatistics( // Return the statistics information of the collection.
                 GetCollectionStatisticsParam.newBuilder()
-                    .withCollectionName(CommonData.defaultCollection)
+                    .withCollectionName(commonCollection)
                     .withFlush(false)
                     .build());
     Assert.assertEquals(respCollectionStatistics.getStatus().intValue(), 0);
@@ -41,7 +62,7 @@ public class GetCollectionStatisticsTest extends BaseTest {
         milvusClient
             .getCollectionStatistics( // Return the statistics information of the collection.
                 GetCollectionStatisticsParam.newBuilder()
-                    .withCollectionName(CommonData.defaultCollection)
+                    .withCollectionName(commonCollection)
                     .withFlush(true)
                     .build());
     Assert.assertEquals(respCollectionStatistics.getStatus().intValue(), 0);
