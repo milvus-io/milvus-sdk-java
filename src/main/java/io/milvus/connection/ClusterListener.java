77c870e6801731a719f901b2cc90a2b4d9c73575
CodeInDreams <hui068323@gmail.com>
2023-06-21 11:58:37 +0800

    Fix unclosed okhttp response (#532)
    (cherry picked from commit 04e971a7caf401bb4c23b87ccdc1e1e779931e3d)
    Signed-off-by: CodeInDreams <hui068323@gmail.com>

--- src/main/java/io/milvus/connection/ClusterListener.java
+++ src/main/java/io/milvus/connection/ClusterListener.java
@@ -34,8 +34,7 @@ public class ClusterListener implements Listener {
                 serverSetting.getServerAddress().getHealthPort());
 
         boolean isRunning = false;
-        try {
-            Response response = get(url);
+        try (Response response = get(url)) {
             isRunning = checkResponse(response);
             if (isRunning) {
                 logger.debug("Host [{}] heartbeat Success of Milvus Cluster Listener.",
