945acaecb3804e987886d13e0b7964e59870d69f
groot <yihua.mo@zilliz.com>
2023-08-23 18:28:24 +0800

    Remove deprecated methods (#598)
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- src/main/java/io/milvus/param/dml/SearchParam.java
+++ src/main/java/io/milvus/param/dml/SearchParam.java
@@ -134,22 +134,6 @@ public class SearchParam {
         }
 
         /**
-         *  Graceful time for BOUNDED Consistency Level
-         *
-         * Note: This parameter is deprecated from Milvus v2.2.9, user only input consistency level to search.
-         *       The time settings of different consistency levels are determined by the server side.
-         *       For this reason, this method is marked as Deprecated in Java SDK v2.2.11
-         *
-         * @param gracefulTime graceful time
-         * @return <code>Builder</code>
-         */
-        @Deprecated
-        public Builder withGracefulTime(Long gracefulTime) {
-            this.gracefulTime = gracefulTime;
-            return this;
-        }
-
-        /**
          * Adds a partition to specify search scope (Optional).
          *
          * @param partitionName partition name
@@ -267,43 +251,6 @@ public class SearchParam {
          */
         public Builder withParams(@NonNull String params) {
             this.params = params;
-            return this;
-        }
-
-        /**
-         * Specify an absolute timestamp in a search to get results based on a data view at a specified point in time.
-         * Default value is 0, server executes search on a full data view.
-         *
-         * @param ts a timestamp value
-         * @return <code>Builder</code>
-         */
-        @Deprecated
-        public Builder withTravelTimestamp(@NonNull Long ts) {
-            this.travelTimestamp = ts;
-            return this;
-        }
-
-        /**
-         * Instructs server to see insert/delete operations performed before a provided timestamp.
-         * If no such timestamp is specified, the server will wait for the latest operation to finish and search.
-         *
-         * Note: The timestamp is not an absolute timestamp, it is a hybrid value combined by UTC time and internal flags.
-         *  We call it TSO, for more information please refer to: https://github.com/milvus-io/milvus/blob/master/docs/design_docs/milvus_hybrid_ts_en.md
-         *  You can get a TSO from insert/delete operations, see the <code>MutationResultWrapper</code> class.
-         *  Use an operation's TSO to set this parameter, the server will execute search after this operation is finished.
-         *
-         * Default value is GUARANTEE_EVENTUALLY_TS, server executes search immediately.
-         *
-         * Note: This parameter is deprecated from Milvus v2.2.9, user only input consistency level to search.
-         *       The time settings of different consistency levels are determined by the server side.
-         *       For this reason, this method is marked as Deprecated in Java SDK v2.2.11
-         *
-         * @param ts a timestamp value
-         * @return <code>Builder</code>
-         */
-        @Deprecated
-        public Builder withGuaranteeTimestamp(@NonNull Long ts) {
-            this.guaranteeTimestamp = ts;
             return this;
         }
 
