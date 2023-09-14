2555a7230c0d31d1fd273c86599ce7ea8cc14e7d
yhmo <yihua.mo@zilliz.com>
2023-09-14 14:41:59 +0800

    Fix the bug that consistency level is ignored in search() interface
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- src/main/java/io/milvus/param/ParamUtils.java
+++ src/main/java/io/milvus/param/ParamUtils.java
@@ -467,6 +467,8 @@ public class ParamUtils {
         // a new parameter from v2.2.9, if user didn't specify consistency level, set this parameter to true
         if (requestParam.getConsistencyLevel() == null) {
             builder.setUseDefaultConsistency(true);
+        } else {
+            builder.setConsistencyLevelValue(requestParam.getConsistencyLevel().getCode());
         }
 
         return builder.build();
@@ -486,6 +488,8 @@ public class ParamUtils {
         // a new parameter from v2.2.9, if user didn't specify consistency level, set this parameter to true
         if (requestParam.getConsistencyLevel() == null) {
             builder.setUseDefaultConsistency(true);
+        } else {
+            builder.setConsistencyLevelValue(requestParam.getConsistencyLevel().getCode());
         }
 
         // set offset and limit value.
