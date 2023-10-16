4aa251bec6076d64dc1a1615ac3ea6aa37eb68e9
“yelusion” <xun.huang@zilliz.com>
2023-10-16 18:06:43 +0800

    fix only cloud serverless cluster port must be 443.
    Signed-off-by: “yelusion” <xun.huang@zilliz.com>

--- src/main/java/io/milvus/common/constant/MilvusClientConstant.java
+++ src/main/java/io/milvus/common/constant/MilvusClientConstant.java
@@ -6,6 +6,8 @@ public class MilvusClientConstant {
         public final static String HOST_HTTPS_PREFIX = "https://";
 
         public final static String HOST_HTTP_PREFIX = "http://";
+
+        public final static String CLOUD_SERVERLESS_URI_REGEX = "^https://in03-.{20,}zilliz.*.com$";
     }
     public static class StringValue {
         public final static String COLON = ":";
