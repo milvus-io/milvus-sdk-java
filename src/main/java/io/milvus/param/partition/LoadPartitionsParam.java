ad7261b0a14681c5172ca3e2fa2d127c0bf016d5
Wei Liu <wei.liu@zilliz.com>
2024-07-02 18:14:09 +0800

    enhance: Remove the logic to set replica_number=1 by default
    Signed-off-by: Wei Liu <wei.liu@zilliz.com>

--- src/main/java/io/milvus/param/partition/LoadPartitionsParam.java
+++ src/main/java/io/milvus/param/partition/LoadPartitionsParam.java
@@ -86,8 +86,8 @@ public class LoadPartitionsParam {
         private Long syncLoadWaitingTimeout = 60L;
 
         // replicaNumber:
-        //   The replica number to load, default by 1
-        private Integer replicaNumber = 1;
+        //   The replica number to load
+        private Integer replicaNumber = 0;
 
         // refresh:
         //   This flag must be set to FALSE when first time call the loadPartitions().
