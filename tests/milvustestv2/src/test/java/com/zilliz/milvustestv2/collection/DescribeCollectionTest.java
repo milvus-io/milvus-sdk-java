398414db4e9bc4d202d71d5c44fd37690c28491e
yongpengli-z <yongpeng.li@zilliz.com>
2024-08-19 16:16:55 +0800

    add testcases abort enable mmap (#1043) (#1044)
    Signed-off-by: yongpengli-z <yongpeng.li@zilliz.com>

--- tests/milvustestv2/src/test/java/com/zilliz/milvustestv2/collection/DescribeCollectionTest.java
+++ tests/milvustestv2/src/test/java/com/zilliz/milvustestv2/collection/DescribeCollectionTest.java
@@ -2,10 +2,22 @@ package com.zilliz.milvustestv2.collection;
 
 import com.zilliz.milvustestv2.common.BaseTest;
 import com.zilliz.milvustestv2.common.CommonData;
+import com.zilliz.milvustestv2.common.CommonFunction;
+import io.milvus.grpc.CollectionSchema;
+import io.milvus.param.Constant;
+import io.milvus.v2.common.DataType;
+import io.milvus.v2.service.collection.request.AlterCollectionReq;
+import io.milvus.v2.service.collection.request.CreateCollectionReq;
 import io.milvus.v2.service.collection.request.DescribeCollectionReq;
+import io.milvus.v2.service.collection.request.DropCollectionReq;
 import io.milvus.v2.service.collection.response.DescribeCollectionResp;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author yongpeng.li
@@ -18,6 +30,25 @@ public class DescribeCollectionTest extends BaseTest {
         DescribeCollectionResp describeCollectionResp = milvusClientV2.describeCollection(DescribeCollectionReq.builder()
                 .collectionName(CommonData.defaultFloatVectorCollection)
                 .build());
-        Assert.assertEquals(describeCollectionResp.getVectorFieldNames().get(0),CommonData.fieldFloatVector);
+        Assert.assertEquals(describeCollectionResp.getVectorFieldNames().get(0), CommonData.fieldFloatVector);
+    }
+
+    @Test(description = "Describe collection when mmap field", groups = {"Smoke"})
+    public void describeMMapCollection() {
+        String newCollection = CommonFunction.createNewCollection(CommonData.dim, null, DataType.FloatVector);
+        Map<String, String> map = new HashMap<String, String>() {{
+            put(Constant.MMAP_ENABLED, "true");
+        }};
+        milvusClientV2.alterCollection(AlterCollectionReq.builder()
+                .collectionName(newCollection)
+                .properties(map).build());
+        DescribeCollectionResp describeCollectionResp = milvusClientV2.describeCollection(DescribeCollectionReq.builder()
+                .collectionName(newCollection)
+                .build());
+        Assert.assertTrue(describeCollectionResp.getProperties().containsKey(Constant.MMAP_ENABLED));
+        Assert.assertTrue(describeCollectionResp.getProperties().get(Constant.MMAP_ENABLED).equalsIgnoreCase("true"));
+        milvusClientV2.dropCollection(DropCollectionReq.builder().collectionName(newCollection).build());
+
+
     }
 }
