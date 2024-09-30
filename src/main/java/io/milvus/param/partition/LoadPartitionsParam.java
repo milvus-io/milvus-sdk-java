a315767547ea356d6dc7fbed9168f3f7377d6ebe
groot <yihua.mo@zilliz.com>
2024-09-30 11:41:11 +0800

    Support partial load (#1092)
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- src/main/java/io/milvus/param/partition/LoadPartitionsParam.java
+++ src/main/java/io/milvus/param/partition/LoadPartitionsParam.java
@@ -46,6 +46,8 @@ public class LoadPartitionsParam {
     private final int replicaNumber;
     private final boolean refresh;
     private final List<String> resourceGroups;
+    private final List<String> loadFields;
+    private final boolean skipLoadDynamicField;
 
     private LoadPartitionsParam(@NonNull Builder builder) {
         this.databaseName = builder.databaseName;
@@ -57,6 +59,8 @@ public class LoadPartitionsParam {
         this.replicaNumber = builder.replicaNumber;
         this.refresh = builder.refresh;
         this.resourceGroups = builder.resourceGroups;
+        this.loadFields = builder.loadFields;
+        this.skipLoadDynamicField = builder.skipLoadDynamicField;
     }
 
     public static Builder newBuilder() {
@@ -99,6 +103,15 @@ public class LoadPartitionsParam {
         //   Specify the target resource groups to load the replicas.
         //   If not specified, the replicas will be loaded into the default resource group.
         private List<String> resourceGroups = new ArrayList<>();
+
+        // loadFields:
+        //   Specify load fields list needed during this load.
+        //   If not specified, all the fields will be loaded.
+        private List<String> loadFields = new ArrayList<>();
+
+        // skipLoadDynamicField:
+        //   Specify whether this load shall skip dynamic schema field.
+        private Boolean skipLoadDynamicField = Boolean.FALSE;
 
         private Builder() {
         }
@@ -224,6 +237,34 @@ public class LoadPartitionsParam {
          */
         public Builder withResourceGroups(@NonNull List<String> resourceGroups) {
             this.resourceGroups.addAll(resourceGroups);
+            return this;
+        }
+
+        /**
+         * Specify load fields list needed during this load.
+         * If not specified, all the fields will be loaded.
+         *
+         * @param loadFields a <code>List</code> of {@link String}
+         * @return <code>Builder</code>
+         */
+        public Builder withLoadFields(@NonNull List<String> loadFields) {
+            loadFields.forEach((field)->{
+                if (!this.loadFields.contains(field)) {
+                    this.loadFields.add(field);
+                }
+            });
+            return this;
+        }
+
+        /**
+         * Specify load fields list needed during this load. If not specified, all the fields will be loaded.
+         * Default is False.
+         *
+         * @param skip <code>Boolean.TRUE</code> skip dynamic field, <code>Boolean.FALSE</code> is not
+         * @return <code>Builder</code>
+         */
+        public Builder withSkipLoadDynamicField(@NonNull Boolean skip) {
+            this.skipLoadDynamicField = skip;
             return this;
         }
 
