46d1512e8190e7d4b5492c06ec0418f0ef25e922
xushuang.hu <108672767+lentitude2tk@users.noreply.github.com>
2024-10-16 16:13:19 +0800

    Support Milvus import-related APIs (#1110)
    Signed-off-by: lentitude2tk <xushuang.hu@zilliz.com>

--- src/main/java/io/milvus/bulkwriter/BulkImport.java
+++ src/main/java/io/milvus/bulkwriter/BulkImport.java
@@ -21,44 +21,43 @@ package io.milvus.bulkwriter;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import io.milvus.bulkwriter.request.BulkImportRequest;
-import io.milvus.bulkwriter.request.GetImportProgressRequest;
-import io.milvus.bulkwriter.request.ListImportJobsRequest;
+import io.milvus.bulkwriter.request.describe.BaseDescribeImportRequest;
+import io.milvus.bulkwriter.request.import_.BaseImportRequest;
+import io.milvus.bulkwriter.request.list.BaseListImportJobsRequest;
 import io.milvus.bulkwriter.response.RestfulResponse;
 
 import java.util.Map;
 
-public class CloudImport extends BaseCloudImport {
+public class BulkImport extends BaseBulkImport {
     private static final Gson GSON_INSTANCE = new Gson();
 
-    public static String bulkImport(String url, String apiKey, BulkImportRequest request) {
+    public static String bulkImport(String url, BaseImportRequest request) {
         String requestURL = url + "/v2/vectordb/jobs/import/create";
 
         Map<String, Object> params = GSON_INSTANCE.fromJson(GSON_INSTANCE.toJson(request), new TypeToken<Map<String, Object>>() {}.getType());
-        String body = postRequest(requestURL, apiKey, params, 60 * 1000);
+        String body = postRequest(requestURL, request.getApiKey(), params, 60 * 1000);
         RestfulResponse<Object> response = GSON_INSTANCE.fromJson(body, new TypeToken<RestfulResponse<Object>>(){}.getType());
         handleResponse(requestURL, response);
         return body;
     }
 
-    public static String getImportProgress(String url, String apiKey, GetImportProgressRequest request) {
+    public static String getImportProgress(String url, BaseDescribeImportRequest request) {
         String requestURL = url + "/v2/vectordb/jobs/import/describe";
 
         Map<String, Object> params = GSON_INSTANCE.fromJson(GSON_INSTANCE.toJson(request), new TypeToken<Map<String, Object>>() {}.getType());
-        String body = postRequest(requestURL, apiKey, params, 60 * 1000);
+        String body = postRequest(requestURL, request.getApiKey(), params, 60 * 1000);
         RestfulResponse<Object> response = GSON_INSTANCE.fromJson(body, new TypeToken<RestfulResponse<Object>>(){}.getType());
         handleResponse(requestURL, response);
         return body;
     }
 
-    public static String listImportJobs(String url, String apiKey, ListImportJobsRequest request) {
+    public static String listImportJobs(String url, BaseListImportJobsRequest request) {
         String requestURL = url + "/v2/vectordb/jobs/import/list";
 
         Map<String, Object> params = GSON_INSTANCE.fromJson(GSON_INSTANCE.toJson(request), new TypeToken<Map<String, Object>>() {}.getType());
-        String body = postRequest(requestURL, apiKey, params, 60 * 1000);
+        String body = postRequest(requestURL, request.getApiKey(), params, 60 * 1000);
         RestfulResponse<Object> response = GSON_INSTANCE.fromJson(body, new TypeToken<RestfulResponse<Object>>(){}.getType());
         handleResponse(requestURL, response);
         return body;
     }
-
 }
