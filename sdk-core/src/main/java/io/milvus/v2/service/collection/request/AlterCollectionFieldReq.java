d0cc72687159acb0d300ae4099a27777884966a2
groot <yihua.mo@zilliz.com>
2024-12-24 18:20:46 +0800

    Support AlterCollectionField (#1252)
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

---
+++ sdk-core/src/main/java/io/milvus/v2/service/collection/request/AlterCollectionFieldReq.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.milvus.v2.service.collection.request;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@SuperBuilder
+public class AlterCollectionFieldReq {
+    private String collectionName;
+    private String fieldName;
+    private String databaseName;
+    @Builder.Default
+    private final Map<String, String> properties = new HashMap<>();
+
+    public static abstract class AlterCollectionFieldReqBuilder<C extends AlterCollectionFieldReq, B extends AlterCollectionFieldReq.AlterCollectionFieldReqBuilder<C, B>> {
+        public B property(String key, String value) {
+            if(null == this.properties$value ){
+                this.properties$value = new HashMap<>();
+            }
+            this.properties$value.put(key, value);
+            this.properties$set = true;
+            return self();
+        }
+    }
+}
