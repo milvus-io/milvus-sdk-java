c8d7933a578623c3b2cfa6159fa4e13183e1dc46
yhmo <yihua.mo@zilliz.com>
2024-12-31 14:37:53 +0800

    QueryIterator/SearchIterator support retry
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- sdk-core/src/main/java/io/milvus/v2/utils/RpcUtils.java
+++ sdk-core/src/main/java/io/milvus/v2/utils/RpcUtils.java
@@ -19,15 +19,25 @@
 
 package io.milvus.v2.utils;
 
+import io.grpc.StatusRuntimeException;
 import io.milvus.grpc.Status;
+import io.milvus.v2.client.RetryConfig;
 import io.milvus.v2.exception.ErrorCode;
 import io.milvus.v2.exception.MilvusClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
 public class RpcUtils {
 
     protected static final Logger logger = LoggerFactory.getLogger(RpcUtils.class);
+    private RetryConfig retryConfig = RetryConfig.builder().build();
+
+    public void retryConfig(RetryConfig retryConfig) {
+        this.retryConfig = retryConfig;
+    }
 
     public void handleResponse(String requestInfo, Status status) {
         // the server made a change for error code:
@@ -54,5 +64,111 @@ public class RpcUtils {
         }
 
         logger.debug("{} successfully!", requestInfo);
+    }
+
+    public <T> T retry(Callable<T> callable) {
+        int maxRetryTimes = retryConfig.getMaxRetryTimes();
+        // no retry, direct call the method
+        if (maxRetryTimes <= 1) {
+            try {
+                return callable.call();
+            } catch (StatusRuntimeException e) {
+                throw new MilvusClientException(ErrorCode.RPC_ERROR, e); // rpc error
+            } catch (MilvusClientException e) {
+                throw e; // server error or client error
+            } catch (Exception e) {
+                throw new MilvusClientException(ErrorCode.CLIENT_ERROR, e); // others error treated as client error
+            }
+        }
+
+        // method to check timeout
+        long begin = System.currentTimeMillis();
+        long maxRetryTimeoutMs = retryConfig.getMaxRetryTimeoutMs();
+        Callable<Boolean> timeoutChecker = ()->{
+            long current = System.currentTimeMillis();
+            long cost = (current - begin);
+            if (maxRetryTimeoutMs > 0 && cost >= maxRetryTimeoutMs) {
+                return Boolean.TRUE;
+            }
+            return Boolean.FALSE;
+        };
+
+        // retry within timeout
+        long retryIntervalMs = retryConfig.getInitialBackOffMs();
+        for (int k = 1; k <= maxRetryTimes; k++) {
+            try {
+                return callable.call();
+            } catch (StatusRuntimeException e) {
+                io.grpc.Status.Code code = e.getStatus().getCode();
+                if (code == io.grpc.Status.DEADLINE_EXCEEDED.getCode()
+                        || code == io.grpc.Status.PERMISSION_DENIED.getCode()
+                        || code == io.grpc.Status.UNAUTHENTICATED.getCode()
+                        || code == io.grpc.Status.INVALID_ARGUMENT.getCode()
+                        || code == io.grpc.Status.ALREADY_EXISTS.getCode()
+                        || code == io.grpc.Status.RESOURCE_EXHAUSTED.getCode()
+                        || code == io.grpc.Status.UNIMPLEMENTED.getCode()) {
+                    String msg = String.format("Encounter rpc error that cannot be retried, reason: %s", e);
+                    logger.error(msg);
+                    throw new MilvusClientException(ErrorCode.RPC_ERROR, msg); // throw rpc error
+                }
+
+                try {
+                    if (timeoutChecker.call() == Boolean.TRUE) {
+                        String msg = String.format("Retry timeout: %dms, maxRetry:%d, retries: %d, reason: %s",
+                                maxRetryTimeoutMs, maxRetryTimes, k, e.getMessage());
+                        logger.warn(msg);
+                        throw new MilvusClientException(ErrorCode.TIMEOUT, msg); // exit retry for timeout
+                    }
+                } catch (Exception ignored) {
+                }
+            } catch (MilvusClientException e) {
+                try {
+                    if (timeoutChecker.call() == Boolean.TRUE) {
+                        String msg = String.format("Retry timeout: %dms, maxRetry:%d, retries: %d, reason: %s",
+                                maxRetryTimeoutMs, maxRetryTimes, k, e.getMessage());
+                        logger.warn(msg);
+                        throw new MilvusClientException(ErrorCode.TIMEOUT, msg); // exit retry for timeout
+                    }
+                } catch (Exception ignored) {
+                }
+
+                // for server-side returned error, only retry for rate limit
+                // in new error codes of v2.3, rate limit error value is 8
+                if (retryConfig.isRetryOnRateLimit() &&
+                        (e.getLegacyServerCode() == io.milvus.grpc.ErrorCode.RateLimit.getNumber() ||
+                                e.getServerErrCode() == 8)) {
+                    // cannot be retried
+                } else {
+                    throw e; // exit retry, throw the error
+                }
+            } catch (Exception e) {
+                throw new MilvusClientException(ErrorCode.CLIENT_ERROR, e); // others error treated as client error
+            }
+
+            try {
+                if (k >= maxRetryTimes) {
+                    // finish retry loop, return the response of the last retry
+                    String msg = String.format("Finish %d retry times, stop retry", maxRetryTimes);
+                    logger.warn(msg);
+                    throw new MilvusClientException(ErrorCode.TIMEOUT, msg); // exceed max time, exit retry
+                } else {
+                    // sleep for interval
+                    // print log, follow the pymilvus logic
+                    if (k > 3) {
+                        logger.warn(String.format("Retry(%d) with interval %dms", k, retryIntervalMs));
+                    }
+                    TimeUnit.MILLISECONDS.sleep(retryIntervalMs);
+                }
+
+                // reset the next interval value
+                retryIntervalMs = retryIntervalMs*retryConfig.getBackOffMultiplier();
+                if (retryIntervalMs > retryConfig.getMaxBackOffMs()) {
+                    retryIntervalMs = retryConfig.getMaxBackOffMs();
+                }
+            } catch (Exception ignored) {
+            }
+        }
+
+        return null;
     }
 }
