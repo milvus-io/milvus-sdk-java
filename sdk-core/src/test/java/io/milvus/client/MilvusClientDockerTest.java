30d9849464c447cfa1b1856cefd72ec776e3dbae
yhmo <yihua.mo@zilliz.com>
2025-05-09 11:54:46 +0800

    Support runAnalyzer() interface
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- sdk-core/src/test/java/io/milvus/client/MilvusClientDockerTest.java
+++ sdk-core/src/test/java/io/milvus/client/MilvusClientDockerTest.java
@@ -75,7 +75,7 @@ class MilvusClientDockerTest {
     private static final TestUtils utils = new TestUtils(DIMENSION);
 
     @Container
-    private static final MilvusContainer milvus = new MilvusContainer("milvusdb/milvus:v2.5.8");
+    private static final MilvusContainer milvus = new MilvusContainer("milvusdb/milvus:v2.5.11");
 
     @BeforeAll
     public static void setUp() {
