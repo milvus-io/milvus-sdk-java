a3180a01b1818fe3ad8f0c337a9f2ab11b38ddc9
xushuang.hu <108672767+lentitude2tk@users.noreply.github.com>
2025-07-28 16:36:54 +0800

    alias interface support database (#1491)
    Signed-off-by: lentitude2tk <xushuang.hu@zilliz.com>

--- sdk-core/src/main/java/io/milvus/v2/service/utility/response/DescribeAliasResp.java
+++ sdk-core/src/main/java/io/milvus/v2/service/utility/response/DescribeAliasResp.java
@@ -25,6 +25,7 @@ import lombok.experimental.SuperBuilder;
 @Data
 @SuperBuilder
 public class DescribeAliasResp {
+    private String databaseName;
     private String collectionName;
     private String alias;
 }
