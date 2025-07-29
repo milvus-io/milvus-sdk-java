c38904d0c4a8b880a59c2be3e8a83b10176584ee
yongpengli-z <yongpeng.li@zilliz.com>
2025-07-29 19:13:31 +0800

    Add testcases for alias interface spport database (#1496)
    Signed-off-by: yongpengli-z <yongpeng.li@zilliz.com>

--- tests/milvustestv2/src/test/java/com/zilliz/milvustestv2/alias/DropAliasTest.java
+++ tests/milvustestv2/src/test/java/com/zilliz/milvustestv2/alias/DropAliasTest.java
@@ -7,6 +7,8 @@ import com.zilliz.milvustestv2.utils.GenerateUtil;
 import io.milvus.v2.common.DataType;
 import io.milvus.v2.common.IndexParam;
 import io.milvus.v2.service.collection.request.DropCollectionReq;
+import io.milvus.v2.service.database.request.CreateDatabaseReq;
+import io.milvus.v2.service.database.request.DropDatabaseReq;
 import io.milvus.v2.service.index.request.CreateIndexReq;
 import io.milvus.v2.service.utility.request.CreateAliasReq;
 import io.milvus.v2.service.utility.request.DropAliasReq;
@@ -29,6 +31,8 @@ public class DropAliasTest extends BaseTest {
 
     String newCollectionName;
     String aliasName;
+    String aliasWithDB;
+    String collectionWithDB;
     @BeforeClass(alwaysRun = true)
     public void providerCollection() {
         aliasName= GenerateUtil.getRandomString(10);
@@ -37,17 +41,32 @@ public class DropAliasTest extends BaseTest {
                 .collectionName(newCollectionName)
                 .alias(aliasName)
                 .build());
+        aliasWithDB = "db_" + GenerateUtil.getRandomString(10);
+        milvusClientV2.createDatabase(CreateDatabaseReq.builder().databaseName(CommonData.databaseName2).build());
+        collectionWithDB = CommonFunction.createNewCollectionWithDatabase(CommonData.dim, null, DataType.FloatVector, CommonData.databaseName2);
+        milvusClientV2.createAlias(CreateAliasReq.builder().alias(aliasWithDB).collectionName(collectionWithDB).databaseName(CommonData.databaseName2).build());
+
     }
 
     @AfterClass(alwaysRun = true)
     public void cleanTestData(){
         milvusClientV2.dropCollection(DropCollectionReq.builder().collectionName(newCollectionName).build());
+        milvusClientV2.dropCollection(DropCollectionReq.builder().collectionName(collectionWithDB).databaseName(CommonData.databaseName2).build());
+        milvusClientV2.dropDatabase(DropDatabaseReq.builder().databaseName(CommonData.databaseName2).build());
     }
 
     @Test(description = "drop alias test",groups = {"Smoke"})
-    public void dropAliaTest(){
+    public void dropAliasTest(){
         milvusClientV2.dropAlias(DropAliasReq.builder().alias(aliasName).build());
         ListAliasResp listAliasResp = milvusClientV2.listAliases(ListAliasesReq.builder().collectionName(newCollectionName).build());
         Assert.assertFalse(listAliasResp.getAlias().contains(aliasName));
     }
+
+    @Test(description = "drop alias with database", groups = {"Smoke"})
+    public void dropAliasWithDatabase() {
+        milvusClientV2.dropAlias(DropAliasReq.builder().alias(aliasWithDB).databaseName(CommonData.databaseName2).build());
+        ListAliasResp listAliasResp = milvusClientV2.listAliases(ListAliasesReq.builder().databaseName(CommonData.databaseName2).collectionName(collectionWithDB).build());
+        Assert.assertFalse(listAliasResp.getAlias().contains(aliasWithDB));
+    }
+
 }
