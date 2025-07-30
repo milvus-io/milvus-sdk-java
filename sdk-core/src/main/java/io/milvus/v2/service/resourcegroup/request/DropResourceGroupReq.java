859a76b3087c97e542ea77538855a14facefea37
kun <kunyinglang@163.com>
2025-07-30 14:49:31 +0800

    first step: remove lombok at io.milvus.v2.service.resourcegroup (#1500)
    Signed-off-by: kun <kunyinglang@163.com>

--- sdk-core/src/main/java/io/milvus/v2/service/resourcegroup/request/DropResourceGroupReq.java
+++ sdk-core/src/main/java/io/milvus/v2/service/resourcegroup/request/DropResourceGroupReq.java
@@ -1,10 +1,61 @@
 package io.milvus.v2.service.resourcegroup.request;
 
-import lombok.Data;
-import lombok.experimental.SuperBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-@Data
-@SuperBuilder
 public class DropResourceGroupReq {
     private String groupName;
+
+    private DropResourceGroupReq(Builder builder) {
+        this.groupName = builder.groupName;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        DropResourceGroupReq that = (DropResourceGroupReq) obj;
+        return new EqualsBuilder()
+                .append(groupName, that.groupName)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(groupName)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "DropResourceGroupReq{" +
+                "groupName='" + groupName + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String groupName;
+
+        public Builder groupName(String groupName) {
+            this.groupName = groupName;
+            return this;
+        }
+
+        public DropResourceGroupReq build() {
+            return new DropResourceGroupReq(this);
+        }
+    }
 }
--- sdk-core/src/main/java/io/milvus/v2/service/resourcegroup/request/ListResourceGroupsReq.java
+++ sdk-core/src/main/java/io/milvus/v2/service/resourcegroup/request/ListResourceGroupsReq.java
@@ -1,9 +1,39 @@
 package io.milvus.v2.service.resourcegroup.request;
 
-import lombok.Data;
-import lombok.experimental.SuperBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-@Data
-@SuperBuilder
 public class ListResourceGroupsReq {
+
+    private ListResourceGroupsReq(Builder builder) {
+        // No fields to initialize
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        return new EqualsBuilder().isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ListResourceGroupsReq{}";
+    }
+
+    public static class Builder {
+
+        public ListResourceGroupsReq build() {
+            return new ListResourceGroupsReq(this);
+        }
+    }
 }
