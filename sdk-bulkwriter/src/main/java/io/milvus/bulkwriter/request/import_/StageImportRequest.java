00af424dc5fc2d92d4105f25f833df2292224276
xushuang.hu <108672767+lentitude2tk@users.noreply.github.com>
2025-08-12 15:21:39 +0800

    bulkImport add objectUrls paramster (#1524)
    Signed-off-by: lentitude2tk <xushuang.hu@zilliz.com>

--- sdk-bulkwriter/src/main/java/io/milvus/bulkwriter/request/import_/StageImportRequest.java
+++ sdk-bulkwriter/src/main/java/io/milvus/bulkwriter/request/import_/StageImportRequest.java
@@ -30,12 +30,50 @@ import java.util.List;
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
+/*
+  If you want to import data into a Zilliz cloud instance and your data is stored in a Zilliz stage,
+  you can use this method to import the data from the stage.
+ */
 public class StageImportRequest extends BaseImportRequest {
-    private String stageName;
-    private List<List<String>> dataPaths;
-
     private String clusterId;
+
+    /**
+     * For Free & Serverless deployments: specifying this parameter is not supported.
+     * For Dedicated deployments: this parameter can be specified; defaults to the "default" database.
+     */
     private String dbName;
     private String collectionName;
+
+    /**
+     * If the collection has partitionKey enabled:
+     *     - The partitionName parameter cannot be specified for import.
+     * If the collection does not have partitionKey enabled:
+     *     - You may specify partitionName for the import.
+     *     - Defaults to the "default" partition if not specified.
+     */
     private String partitionName;
+
+    private String stageName;
+
+    /**
+     * Data import can be configured in multiple ways using `dataPaths`:
+     * <p>
+     * 1. Multi-path import (multiple folders or files):
+     *    "dataPaths": [
+     *        ["parquet-folder-1/1.parquet"],
+     *        ["parquet-folder-2/1.parquet"],
+     *        ["parquet-folder-3/"]
+     *    ]
+     * <p>
+     * 2. Folder import:
+     *    "dataPaths": [
+     *        ["parquet-folder/"]
+     *    ]
+     * <p>
+     * 3. Single file import:
+     *    "dataPaths": [
+     *        ["parquet-folder/1.parquet"]
+     *    ]
+     */
+    private List<List<String>> dataPaths;
 }
