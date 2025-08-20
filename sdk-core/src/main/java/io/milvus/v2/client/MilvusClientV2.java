1f41fe18b513f2f87801974779d3f1c46c1bf028
uncle-cyh <42989534+uncle-cyh@users.noreply.github.com>
2025-08-20 19:09:40 +0800

    fix:remove collection schema cache after dropCollection (#1550)
    Signed-off-by: uncle <772817297@qq.com>
    Co-authored-by: uncle <772817297@qq.com>

--- sdk-core/src/main/java/io/milvus/v2/client/MilvusClientV2.java
+++ sdk-core/src/main/java/io/milvus/v2/client/MilvusClientV2.java
@@ -320,6 +320,7 @@ public class MilvusClientV2 {
      */
     public void dropCollection(DropCollectionReq request) {
         rpcUtils.retry(()-> collectionService.dropCollection(this.getRpcStub(), request));
+        vectorService.removeCollectionCache(request.getDatabaseName(), request.getCollectionName());
     }
     /**
      * Alter a collection in Milvus.
