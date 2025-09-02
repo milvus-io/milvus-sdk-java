cef9934eb901153af6f2a25d73044e0678a6a127
yhmo <yihua.mo@zilliz.com>
2025-09-02 14:30:34 +0800

    Fix a defect of QueryReq
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- examples/src/main/java/io/milvus/v2/JsonFieldExample.java
+++ examples/src/main/java/io/milvus/v2/JsonFieldExample.java
@@ -147,7 +147,6 @@ public class JsonFieldExample {
         // Get row count, set ConsistencyLevel.STRONG to sync the data to query node so that data is visible
         QueryResp countR = client.query(QueryReq.builder()
                 .collectionName(COLLECTION_NAME)
-                .filter("")
                 .outputFields(Collections.singletonList("count(*)"))
                 .consistencyLevel(ConsistencyLevel.STRONG)
                 .build());
