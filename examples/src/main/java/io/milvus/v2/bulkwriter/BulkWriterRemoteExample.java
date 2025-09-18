accac09cef209649aa92ce3b7f8048ceb70d7d34
lentitude2tk <xushuang.hu@zilliz.com>
2025-09-18 20:37:29 +0800

    bulk_import add apiKey note
    Signed-off-by: lentitude2tk <xushuang.hu@zilliz.com>

--- examples/src/main/java/io/milvus/v2/bulkwriter/BulkWriterRemoteExample.java
+++ examples/src/main/java/io/milvus/v2/bulkwriter/BulkWriterRemoteExample.java
@@ -442,6 +442,7 @@ public class BulkWriterRemoteExample {
         MilvusImportRequest milvusImportRequest = MilvusImportRequest.builder()
                 .collectionName(ALL_TYPES_COLLECTION_NAME)
                 .files(batchFiles)
+                .apiKey(USER_NAME + ":" + PASSWORD)
                 .options(options)
                 .build();
         String bulkImportResult = BulkImportUtils.bulkImport(url, milvusImportRequest);
@@ -452,7 +453,9 @@ public class BulkWriterRemoteExample {
         System.out.println("Create a bulkInert task, job id: " + jobId);
 
         System.out.println("\n===================== listBulkInsertJobs() ====================");
-        MilvusListImportJobsRequest listImportJobsRequest = MilvusListImportJobsRequest.builder().collectionName(ALL_TYPES_COLLECTION_NAME).build();
+        MilvusListImportJobsRequest listImportJobsRequest = MilvusListImportJobsRequest.builder().collectionName(ALL_TYPES_COLLECTION_NAME)
+                .apiKey(USER_NAME + ":" + PASSWORD)
+                .build();
         String listImportJobsResult = BulkImportUtils.listImportJobs(url, listImportJobsRequest);
         System.out.println(listImportJobsResult);
         while (true) {
@@ -462,6 +465,7 @@ public class BulkWriterRemoteExample {
             System.out.println("\n===================== getBulkInsertState() ====================");
             MilvusDescribeImportRequest request = MilvusDescribeImportRequest.builder()
                     .jobId(jobId)
+                    .apiKey(USER_NAME + ":" + PASSWORD)
                     .build();
             String getImportProgressResult = BulkImportUtils.getImportProgress(url, request);
             System.out.println(getImportProgressResult);
