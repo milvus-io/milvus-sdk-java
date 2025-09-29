81566c959138334469e8ab067774bf9e052a5578
yhmo <yihua.mo@zilliz.com>
2025-09-29 19:10:41 +0800

    Add geometry example
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- sdk-core/src/test/java/io/milvus/TestUtils.java
+++ sdk-core/src/test/java/io/milvus/TestUtils.java
@@ -11,7 +11,7 @@ public class TestUtils {
     private int dimension = 256;
     private static final Random RANDOM = new Random();
 
-    public static final String MilvusDockerImageID = "milvusdb/milvus:master-20250927-cc53b25b";
+    public static final String MilvusDockerImageID = "milvusdb/milvus:master-20250929-ca1cc7c9-amd64";
 
     public TestUtils(int dimension) {
         this.dimension = dimension;
