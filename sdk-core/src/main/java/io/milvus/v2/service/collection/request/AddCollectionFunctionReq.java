9accaebeb19007800ee95ec876433dcc943ae9ca
groot <yihua.mo@zilliz.com>
2025-12-04 17:39:07 +0800

    Support AddCollectionFunction/AlterCollectionFunction/DropCollectionFunction (#1708)
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

---
+++ sdk-core/src/main/java/io/milvus/v2/service/collection/request/AddCollectionFunctionReq.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.milvus.v2.service.collection.request;
+
+public class AddCollectionFunctionReq {
+    private String collectionName;
+    private String databaseName;
+    private CreateCollectionReq.Function function;
+
+    private AddCollectionFunctionReq(AddCollectionFunctionReqBuilder builder) {
+        this.collectionName = builder.collectionName;
+        this.databaseName = builder.databaseName;
+        this.function = builder.function;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public CreateCollectionReq.Function getFunction() {
+        return function;
+    }
+
+    @Override
+    public String toString() {
+        return "AddCollectionFunctionReq{" +
+                "collectionName='" + collectionName + '\'' +
+                ", databaseName='" + databaseName + '\'' +
+                ", function= " + function +
+                '}';
+    }
+
+    public static AddCollectionFunctionReqBuilder builder() {
+        return new AddCollectionFunctionReqBuilder();
+    }
+
+    public static class AddCollectionFunctionReqBuilder {
+        private String collectionName = "";
+        private String databaseName = "";
+        private CreateCollectionReq.Function function;
+
+        private AddCollectionFunctionReqBuilder() {
+        }
+
+        public AddCollectionFunctionReqBuilder collectionName(String collectionName) {
+            this.collectionName = collectionName;
+            return this;
+        }
+
+        public AddCollectionFunctionReqBuilder databaseName(String databaseName) {
+            this.databaseName = databaseName;
+            return this;
+        }
+
+        public AddCollectionFunctionReqBuilder function(CreateCollectionReq.Function function) {
+            this.function = function;
+            return this;
+        }
+
+        public AddCollectionFunctionReq build() {
+            return new AddCollectionFunctionReq(this);
+        }
+    }
+}
