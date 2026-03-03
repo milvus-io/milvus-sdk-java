26484ca8dc79f04f83b7ca3cc0b8a6795012ef63
许晓峰 <neil.xuxf@gmail.com>
2026-03-03 17:57:17 +0800

    DescribeCollection fieldNames/vectorFieldNames for Array<Struct> schema (#1763)
    * fix(v2): include structFields in DescribeCollection fieldNames
    
    - Merge fieldsList and structArrayFieldsList when building DescribeCollectionResp.fieldNames
    
    Signed-off-by: xuxiaofeng <xuxiaofeng.work@bytedance.com>
    
    * fix(v2): include struct vector subfields in DescribeCollection vectorFieldNames
    
    - Build vectorFieldNames from both fieldsList and structArrayFieldsList
    - For Array<Struct>, append vector subfields as "parent[subField]" (e.g. clips[vec])
    
    Signed-off-by: xuxiaofeng <xuxiaofeng.work@bytedance.com>
    
    ---------
    
    Signed-off-by: xuxiaofeng <xuxiaofeng.work@bytedance.com>
    Co-authored-by: xuxiaofeng <xuxiaofeng.work@bytedance.com>

---
+++ sdk-core/src/test/java/io/milvus/v2/utils/ConvertUtilsTest.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.milvus.v2.utils;
+
+import io.milvus.grpc.CollectionSchema;
+import io.milvus.grpc.ConsistencyLevel;
+import io.milvus.grpc.DataType;
+import io.milvus.grpc.DescribeCollectionResponse;
+import io.milvus.grpc.FieldSchema;
+import io.milvus.grpc.StructArrayFieldSchema;
+import io.milvus.v2.service.collection.request.AddFieldReq;
+import io.milvus.v2.service.collection.request.CreateCollectionReq;
+import io.milvus.v2.service.collection.response.DescribeCollectionResp;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConvertUtilsTest {
+    @Test
+    void testConvertDescCollectionRespFieldNamesIncludeStructFields() {
+        FieldSchema idField = FieldSchema.newBuilder()
+                .setName("id")
+                .setDataType(DataType.Int64)
+                .setIsPrimaryKey(true)
+                .build();
+
+        FieldSchema vectorField = FieldSchema.newBuilder()
+                .setName("vector")
+                .setDataType(DataType.FloatVector)
+                .build();
+
+        CreateCollectionReq.StructFieldSchema structFieldSchema = CreateCollectionReq.StructFieldSchema.builder()
+                .name("clips")
+                .maxCapacity(10)
+                .build();
+        structFieldSchema.addField(AddFieldReq.builder()
+                .fieldName("vec")
+                .dataType(io.milvus.v2.common.DataType.FloatVector)
+                .dimension(8)
+                .build());
+
+        StructArrayFieldSchema rpcStructFieldSchema = SchemaUtils.convertToGrpcStructFieldSchema(structFieldSchema);
+
+        CollectionSchema schema = CollectionSchema.newBuilder()
+                .setEnableDynamicField(false)
+                .addFields(idField)
+                .addFields(vectorField)
+                .addStructArrayFields(rpcStructFieldSchema)
+                .build();
+
+        DescribeCollectionResponse response = DescribeCollectionResponse.newBuilder()
+                .setCollectionName("test")
+                .setCollectionID(1L)
+                .setDbName("default")
+                .setSchema(schema)
+                .setNumPartitions(1)
+                .setCreatedTimestamp(0L)
+                .setCreatedUtcTimestamp(0L)
+                .setConsistencyLevel(ConsistencyLevel.Bounded)
+                .setShardsNum(1)
+                .build();
+
+        DescribeCollectionResp resp = new ConvertUtils().convertDescCollectionResp(response);
+        Assertions.assertTrue(resp.getFieldNames().contains("id"));
+        Assertions.assertTrue(resp.getFieldNames().contains("vector"));
+        Assertions.assertTrue(resp.getFieldNames().contains("clips"));
+        Assertions.assertTrue(resp.getVectorFieldNames().contains("vector"));
+        Assertions.assertTrue(resp.getVectorFieldNames().contains("clips[vec]"));
+    }
+}
