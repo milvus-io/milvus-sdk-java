7df069a0f0b70a98f1ca80023685353a4ce13a48
yhmo <yihua.mo@zilliz.com>
2026-05-09 17:59:54 +0800

    Use milvus 3.0 container to test
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- sdk-core/src/test/java/io/milvus/client/MilvusClientDockerTest.java
+++ sdk-core/src/test/java/io/milvus/client/MilvusClientDockerTest.java
@@ -92,8 +92,13 @@ class MilvusClientDockerTest {
     }
 
     @Container
-    private static final MilvusContainer milvus = new MilvusContainer(TestUtils.MilvusDockerImageID)
-            .withEnv("DEPLOY_MODE", "STANDALONE");
+    private static final MilvusContainer milvus = new MilvusContainer(TestUtils.MilvusDockerImageID) {
+        @Override
+        protected void configure() {
+            super.configure();
+            getEnvMap().remove("ETCD_CONFIG_PATH");
+        }
+    }.withEnv("DEPLOY_MODE", "STANDALONE");
 
     @BeforeAll
     public static void setUp() {
