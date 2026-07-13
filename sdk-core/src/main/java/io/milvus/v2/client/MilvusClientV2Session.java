ce5dec80be5c9a9ebf9d3940f63faafced3cb3db
yhmo <yihua.mo@zilliz.com>
2026-07-13 17:25:51 +0800

    Simplify MilvusClientV2Session class
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- sdk-core/src/main/java/io/milvus/v2/client/MilvusClientV2Session.java
+++ sdk-core/src/main/java/io/milvus/v2/client/MilvusClientV2Session.java
@@ -28,7 +28,6 @@ import io.milvus.v2.service.vector.request.*;
 import io.milvus.v2.service.vector.response.GetResp;
 import io.milvus.v2.service.vector.response.QueryResp;
 import io.milvus.v2.service.vector.response.SearchResp;
-import org.apache.commons.lang3.StringUtils;
 
 public class MilvusClientV2Session {
     private final MilvusClientV2 parent;
@@ -40,39 +39,50 @@ public class MilvusClientV2Session {
         this.clusterId = clusterId;
     }
 
+    public String getClusterId() {
+        return clusterId;
+    }
+
     public SearchResp search(SearchReq request) {
         ensureOpen();
-        return parent.search(copy(request));
+        request.setClusterId(clusterId);
+        return parent.search(request);
     }
 
     public SearchResp hybridSearch(HybridSearchReq request) {
         ensureOpen();
-        return parent.hybridSearch(copy(request));
+        request.setClusterId(clusterId);
+        return parent.hybridSearch(request);
     }
 
     public QueryResp query(QueryReq request) {
         ensureOpen();
-        return parent.query(copy(request));
+        request.setClusterId(clusterId);
+        return parent.query(request);
     }
 
     public QueryIterator queryIterator(QueryIteratorReq request) {
         ensureOpen();
-        return parent.queryIterator(copy(request));
+        request.setClusterId(clusterId);
+        return parent.queryIterator(request);
     }
 
     public SearchIterator searchIterator(SearchIteratorReq request) {
         ensureOpen();
-        return parent.searchIterator(copy(request));
+        request.setClusterId(clusterId);
+        return parent.searchIterator(request);
     }
 
     public SearchIteratorV2 searchIteratorV2(SearchIteratorReqV2 request) {
         ensureOpen();
-        return parent.searchIteratorV2(copy(request));
+        request.setClusterId(clusterId);
+        return parent.searchIteratorV2(request);
     }
 
     public GetResp get(GetReq request) {
         ensureOpen();
-        return parent.get(copy(request));
+        request.setClusterId(clusterId);
+        return parent.get(request);
     }
 
     public void close() {
@@ -83,166 +93,5 @@ public class MilvusClientV2Session {
         if (closed) {
             throw new MilvusClientException(ErrorCode.INVALID_PARAMS, "MilvusClient session is closed");
         }
-    }
-
-    private void checkClusterId(String requestClusterId) {
-        if (StringUtils.isNotEmpty(requestClusterId) && !clusterId.equals(requestClusterId)) {
-            throw new MilvusClientException(ErrorCode.INVALID_PARAMS, "clusterId conflicts with session clusterId");
-        }
-    }
-
-    private SearchReq copy(SearchReq request) {
-        checkClusterId(request.getClusterId());
-        return SearchReq.builder()
-                .databaseName(request.getDatabaseName())
-                .collectionName(request.getCollectionName())
-                .clusterId(clusterId)
-                .partitionNames(request.getPartitionNames())
-                .annsField(request.getAnnsField())
-                .metricType(request.getMetricType())
-                .filter(request.getFilter())
-                .outputFields(request.getOutputFields())
-                .data(request.getData())
-                .ids(request.getIds())
-                .offset(request.getOffset())
-                .limit(request.getLimit())
-                .roundDecimal(request.getRoundDecimal())
-                .searchParams(request.getSearchParams())
-                .guaranteeTimestamp(request.getGuaranteeTimestamp())
-                .gracefulTime(request.getGracefulTime())
-                .consistencyLevel(request.getConsistencyLevel())
-                .ignoreGrowing(request.isIgnoreGrowing())
-                .timezone(request.getTimezone())
-                .orderByFields(request.getOrderByFields())
-                .groupByFieldName(request.getGroupByFieldName())
-                .groupSize(request.getGroupSize())
-                .strictGroupSize(request.getStrictGroupSize())
-                .ranker(request.getRanker())
-                .functionScore(request.getFunctionScore())
-                .filterTemplateValues(request.getFilterTemplateValues())
-                .highlighter(request.getHighlighter())
-                .searchAggregation(request.getSearchAggregation())
-                .build();
-    }
-
-    private HybridSearchReq copy(HybridSearchReq request) {
-        checkClusterId(request.getClusterId());
-        return HybridSearchReq.builder()
-                .databaseName(request.getDatabaseName())
-                .collectionName(request.getCollectionName())
-                .clusterId(clusterId)
-                .partitionNames(request.getPartitionNames())
-                .searchRequests(request.getSearchRequests())
-                .ranker(request.getRanker())
-                .functionScore(request.getFunctionScore())
-                .limit(request.getLimit())
-                .outFields(request.getOutFields())
-                .offset(request.getOffset())
-                .roundDecimal(request.getRoundDecimal())
-                .consistencyLevel(request.getConsistencyLevel())
-                .groupByFieldName(request.getGroupByFieldName())
-                .groupSize(request.getGroupSize())
-                .strictGroupSize(request.getStrictGroupSize())
-                .build();
-    }
-
-    private QueryReq copy(QueryReq request) {
-        checkClusterId(request.getClusterId());
-        return QueryReq.builder()
-                .databaseName(request.getDatabaseName())
-                .collectionName(request.getCollectionName())
-                .clusterId(clusterId)
-                .partitionNames(request.getPartitionNames())
-                .outputFields(request.getOutputFields())
-                .ids(request.getIds())
-                .filter(request.getFilter())
-                .consistencyLevel(request.getConsistencyLevel())
-                .offset(request.getOffset())
-                .limit(request.getLimit())
-                .ignoreGrowing(request.isIgnoreGrowing())
-                .timezone(request.getTimezone())
-                .orderByFields(request.getOrderByFields())
-                .queryParams(request.getQueryParams())
-                .filterTemplateValues(request.getFilterTemplateValues())
-                .build();
-    }
-
-    private QueryIteratorReq copy(QueryIteratorReq request) {
-        checkClusterId(request.getClusterId());
-        return QueryIteratorReq.builder()
-                .databaseName(request.getDatabaseName())
-                .collectionName(request.getCollectionName())
-                .clusterId(clusterId)
-                .partitionNames(request.getPartitionNames())
-                .outputFields(request.getOutputFields())
-                .expr(request.getExpr())
-                .consistencyLevel(request.getConsistencyLevel())
-                .offset(request.getOffset())
-                .limit(request.getLimit())
-                .ignoreGrowing(request.isIgnoreGrowing())
-                .timezone(request.getTimezone())
-                .batchSize(request.getBatchSize())
-                .reduceStopForBest(request.isReduceStopForBest())
-                .filterTemplateValues(request.getFilterTemplateValues())
-                .build();
-    }
-
-    private SearchIteratorReq copy(SearchIteratorReq request) {
-        checkClusterId(request.getClusterId());
-        return SearchIteratorReq.builder()
-                .databaseName(request.getDatabaseName())
-                .collectionName(request.getCollectionName())
-                .clusterId(clusterId)
-                .partitionNames(request.getPartitionNames())
-                .metricType(request.getMetricType())
-                .vectorFieldName(request.getVectorFieldName())
-                .limit(request.getLimit())
-                .expr(request.getExpr())
-                .outputFields(request.getOutputFields())
-                .vectors(request.getVectors())
-                .roundDecimal(request.getRoundDecimal())
-                .params(request.getParams())
-                .consistencyLevel(request.getConsistencyLevel())
-                .ignoreGrowing(request.isIgnoreGrowing())
-                .groupByFieldName(request.getGroupByFieldName())
-                .batchSize(request.getBatchSize())
-                .build();
-    }
-
-    private SearchIteratorReqV2 copy(SearchIteratorReqV2 request) {
-        checkClusterId(request.getClusterId());
-        return SearchIteratorReqV2.builder()
-                .databaseName(request.getDatabaseName())
-                .collectionName(request.getCollectionName())
-                .clusterId(clusterId)
-                .partitionNames(request.getPartitionNames())
-                .metricType(request.getMetricType())
-                .vectorFieldName(request.getVectorFieldName())
-                .limit(request.getLimit())
-                .filter(request.getFilter())
-                .outputFields(request.getOutputFields())
-                .vectors(request.getVectors())
-                .roundDecimal(request.getRoundDecimal())
-                .searchParams(request.getSearchParams())
-                .consistencyLevel(request.getConsistencyLevel())
-                .ignoreGrowing(request.isIgnoreGrowing())
-                .timezone(request.getTimezone())
-                .groupByFieldName(request.getGroupByFieldName())
-                .batchSize(request.getBatchSize())
-                .externalFilterFunc(request.getExternalFilterFunc())
-                .filterTemplateValues(request.getFilterTemplateValues())
-                .build();
-    }
-
-    private GetReq copy(GetReq request) {
-        checkClusterId(request.getClusterId());
-        return GetReq.builder()
-                .databaseName(request.getDatabaseName())
-                .collectionName(request.getCollectionName())
-                .clusterId(clusterId)
-                .partitionName(request.getPartitionName())
-                .ids(request.getIds())
-                .outputFields(request.getOutputFields())
-                .build();
     }
 }
